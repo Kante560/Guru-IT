1569a353f0d4702f892f9c0824ddb612ca606c75
Alivexem <athkinstestimony@gmail.com>
2025-09-26 11:40:05 +0100

    'updated'

--- src/Pages/admin/AdminAssignmentsList.tsx
+++ src/Pages/admin/AdminAssignmentsList.tsx
@@ -42,7 +42,7 @@ const AdminAssignmentsList = () => {
   }, [token]);
 
   return (
-    <div className="min-h-screen bg-gray-100 pt-20 font-inter">
+    <div className="min-h-screen  bg-gray-100 pt-20 font-inter">
       <AdminNav />
       <div className="max-w-7xl mx-auto p-2 sm:p-6 mt-8 sm:mt-12 w-full">
         <h2 className="text-lg sm:text-2xl font-semibold mb-4 sm:mb-6 text-center">Submitted Assignments</h2>
